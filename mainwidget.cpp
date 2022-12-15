fbbbba77c67ddc96b13e35b02df607b071cbc01c
我不是罗大锤 <1696547463@qq.com>
2022-12-15 22:34:08 +0800

    修改图标，完善登录页布局

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,5 +1,6 @@
 #include "mainwidget.h"
 #include "ui_mainwidget.h"
+#include "QMessageBox"
 
 MainWidget::MainWidget(QWidget *parent) :
     QWidget(parent),
@@ -12,4 +13,13 @@ MainWidget::MainWidget(QWidget *parent) :
 MainWidget::~MainWidget()
 {
     delete ui;
+}
+
+void MainWidget::on_login_btn_clicked()
+{
+    QString username = ui->username_edit->toPlainText();
+    QString password = ui->password_edit->text();
+    if (username.isEmpty() || password.isEmpty()) {
+        QMessageBox::information(this, "温馨提示", "请将内容输入完整", "确定");
+    }
 }
